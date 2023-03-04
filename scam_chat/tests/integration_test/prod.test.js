86a28c32ffc46d81c81b13226433eea2db044897
charlotte-wt <charlottewee01@gmail.com>
2023-03-05 01:53:29 +0800

    fix: modified eslint configurations for test files - fixed linting issues for server.js as well

--- scam_chat/tests/integration_test/prod.test.js
+++ scam_chat/tests/integration_test/prod.test.js
@@ -5,8 +5,7 @@ import mongoose from 'mongoose';
 
 dotenv.config({ path: '../../.env' });
 
-const request = supertest.agent(app)
-
+const request = supertest.agent(app);
 
 /* Connecting to the database before each test. */
 beforeEach(async () => {
@@ -19,8 +18,6 @@ afterEach(async () => {
   await mongoose.connection.db.dropDatabase();
   await mongoose.connection.close();
 });
-
-
 
 describe('Health Check: GET /', () => {
   it('should return healthy status', async () => {
@@ -40,7 +37,6 @@ describe('POST /tele_user/', () => {
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.phone_num).toBe('+6512345678');
-
   });
 });
 
@@ -57,80 +53,72 @@ describe('GET /tele_user/:phone_num', () => {
     expect(res.statusCode).toBe(200);
     expect(res.body[0].phone_num).toBe('+6512345678');
   });
-
 });
-
 
 describe('POST /chat/createChat', () => {
   it('should return created 201 status', async () => {
     const res = await request.post('/chat/createChat').send({
-      'phone_num': '+6512345678',
-      'contact_name': 'Hello',
-      'total_msgs': 10,
-      'chat_id': '12345678910'
+      phone_num: '+6512345678',
+      contact_name: 'Hello',
+      total_msgs: 10,
+      chat_id: '12345678910'
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.phone_num).toBe('+6512345678');
-
   });
 });
 
 describe('GET /chat/:phone_num/:chat_id', () => {
   it('should return correct chat details', async () => {
-
     await request.post('/chat/createChat').send({
-      'phone_num': '+6512345678',
-      'contact_name': 'Hello',
-      'total_msgs': 10,
-      'chat_id': '12345678910'
+      phone_num: '+6512345678',
+      contact_name: 'Hello',
+      total_msgs: 10,
+      chat_id: '12345678910'
     });
 
     const res = await request.get('/chat/get_by_id/+6512345678/12345678910');
     expect(res.statusCode).toBe(200);
     expect(res.body[0].contact_name).toBe('Hello');
   });
-
 });
 
 describe('POST /msg', () => {
   it('should return created 201 status', async () => {
     const res = await request.post('/msg').send({
-      'phone_num':'+6512345678',
-      'chat_id':'12345678910',
-      'msg_id':'1000',
-      'sender_username':'CharlyCharyChar',
-      'sender_id':'511302430',
-      'sender_firstname':'Charlotte',
-      'text':'hi',
-      'type':0,
-      'date':'3/4/2023',
-      'time':'6:21 PM'
-  });
+      phone_num: '+6512345678',
+      chat_id: '12345678910',
+      msg_id: '1000',
+      sender_username: 'CharlyCharyChar',
+      sender_id: '511302430',
+      sender_firstname: 'Charlotte',
+      text: 'hi',
+      type: 0,
+      date: '3/4/2023',
+      time: '6:21 PM'
+    });
     expect(res.statusCode).toBe(201);
     expect(res.body.phone_num).toBe('+6512345678');
-
   });
 });
 
 describe('GET /msg/get_msg/:phone_num/:chat_id/:msg_id', () => {
   it('should return correct msg details', async () => {
-
     await request.post('/msg').send({
-      'phone_num':'+6512345678',
-      'chat_id':'12345678910',
-      'msg_id':'1000',
-      'sender_username':'CharlyCharyChar',
-      'sender_id':'511302430',
-      'sender_firstname':'Charlotte',
-      'text':'hi',
-      'type':0,
-      'date':'3/4/2023',
-      'time':'6:21 PM'
+      phone_num: '+6512345678',
+      chat_id: '12345678910',
+      msg_id: '1000',
+      sender_username: 'CharlyCharyChar',
+      sender_id: '511302430',
+      sender_firstname: 'Charlotte',
+      text: 'hi',
+      type: 0,
+      date: '3/4/2023',
+      time: '6:21 PM'
     });
 
     const res = await request.get('/msg/get_msg/+6512345678/12345678910/1000');
     expect(res.statusCode).toBe(200);
     expect(res.body[0].text).toBe('hi');
   });
-
 });
--- scam_chat/tests/integration_test/test.server.js
+++ scam_chat/tests/integration_test/test.server.js
@@ -11,7 +11,6 @@ import MessageRoute from '../../app/routes/message.route.js';
 
 const app = express();
 
-
 // middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -24,7 +23,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const CONNECTION = process.env.TESTDB_URI;
-
 
 mongoose
   .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
