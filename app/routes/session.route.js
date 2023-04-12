3f67dc67bef12f7fd50efa1735846ae3cd6c693c
Emmanuel Oh <emmaneugene@outlook.com>
2023-04-12 09:52:12 +0800

    feat: Add `createSession` route

--- app/routes/session.route.js
+++ app/routes/session.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getSessions, getSessionsAvailable, getSessionsByUserId } from '../controllers/session.controller.js';
+import { getSessions, getSessionsAvailable, getSessionsByUserId, createSession } from '../controllers/session.controller.js';
 
 const router = express.Router();
 
 router.get('/sessions/all', getSessions);
 router.get('/sessions/open', getSessionsAvailable);
 router.get('/sessions/:user_id', getSessionsByUserId);
+
+router.post('/sessions', createSession);
 
 export default router;
